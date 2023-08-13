6e3f1fe26b6b8d6654270ed07211fd0dc2909b49
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-08-13 13:10:46 +0530

    Add vuetify to the project.

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
